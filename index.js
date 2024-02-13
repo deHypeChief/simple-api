4d5a774f9a17468da8b298f9c7a9d3ff1ab96257
deHypeChief <dev.hype7@gmail.com>
2024-02-13 20:33:15 +0100

    api update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 
